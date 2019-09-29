bf480a76cd7c66a9b0e656198e95f9f93c4e93c0
Thomas Guntenaar <thomas@guntenaar.org>
2019-09-29 12:31:54 +0200

    affiliate-wp basic css

--- wp-content/themes/leadengine/dashboard/pages/profile/affiliate_area.php
+++ wp-content/themes/leadengine/dashboard/pages/profile/affiliate_area.php
@@ -17,8 +17,17 @@
   <title>Affiliate Area</title>
 </head>
   <body>
-    <?php
-      echo do_shortcode("[affiliate_area]");
-    ?>
+    <div class="content-right y-scroll col-lg-9" style="padding-left: 25px;">
+      <div class="acitivities col-lg-12">
+        <div id="profile-member">
+          <h3 class="h3-fix">Affiliate Area</h3>
+          <?php echo do_shortcode("[affiliate_area]"); ?>
+          <!-- TODO: itjes -->
+          <!-- <div class="profile-exp">
+            <i id="profile-exp" class="information fas fa-info"></i>
+          </div> -->
+        </div>
+      </div>
+    </div>
   </body>
 </html>
