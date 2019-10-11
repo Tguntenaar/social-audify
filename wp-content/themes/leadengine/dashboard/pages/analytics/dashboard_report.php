10cc419e288a641e3b9782831e7b8c676b988c5c
Daan Kuyper <daan_kuyper@hotmail.nl>
2019-10-11 21:09:29 +0200

    Changes

--- wp-content/themes/leadengine/dashboard/pages/analytics/dashboard_report.php
+++ wp-content/themes/leadengine/dashboard/pages/analytics/dashboard_report.php
@@ -23,52 +23,83 @@
 <body>
 
 <div class="a-container col-lg-12">
-    <h2>Report overview</h2>
-    <div class="stat-container-a col-lg-12">
-        <?php
-            $reports = $report_control->get_all_reports();
-            $report_count = sizeof($reports);
-            $report_view_count = 0;
-            $report_today_count = 0;
+  <h2>User overview</h2>
+  <div class="stat-container-a col-lg-12"> <?php
+    $audits = $audit_control->get_all_audits();
+    $count = sizeof($audits);
+    $view_count = 0;
+    $today_count = 0;
 
-            foreach($reports as $report) {
-                $user = $user_control->get((int)$client_control->get($report->client_id)->user_id)->name;
-                $earlier = new DateTime($report->create_date);
-                $later = new DateTime(date('Y-m-d H:i:s'));
-                $day_difference = $later->diff($earlier)->format("%a");
+    foreach($audits as $audit) {
+      $user = $user_control->get((int)$client_control->get($report->client_id)->user_id)->name;
+      if (date('Ymd') == date('Ymd', strtotime($audit->create_date))) {
+        $audit_today_count++;
+      }
 
-                if($day_difference == 0) {
-                    $report_today_count++;
-                }
+      if ($audit->view_time == NULL) {
+        $viewed = "not viewed";
+      } else {
+        $viewed = "viewed";
+        $view_count++;
+      }
 
-                if($report->view_time == NULL) {
-                    $viewed = "not viewed";
-                } else {
-                    $viewed = "viewed";
-                    $report_view_count++;
-                }
+      $slug = strtolower('audit-'.$audit->name.'-'.$audit->id.'/'); ?>
+      <a href="<?php echo $slug; ?>" class="a-row col-xs-12 col-ms-12 col-md-12 col-lg-12">
+        <div class='a-col col-xs-2 col-ms-2 col-md-2 col-lg-2'><?php echo $user; ?></div>
+        <div class='a-col col-xs-2 col-ms-2 col-md-2 col-lg-2'><?php echo $audit->name; ?></div>
+        <div class='a-col col-xs-2 col-ms-2 col-md-2 col-lg-2'><?php echo $viewed; ?></div>
+        <div class='a-col col-xs-2 col-ms-2 col-md-2 col-lg-3'><?php echo $audit->create_date; ?></div>
+      </a><?php
+    } ?>
+  </div>
 
+  <span class="analytics-stats">
+    Viewed audits: <strong><?php echo $view_count; ?></strong> / <?php echo $audit_count; ?>
+  </span>
 
-                $slug = strtolower('report-'.$report->name.'-'.$report->id.'/');
+  <span class="analytics-stats">
+    Audits created today: <strong><?php echo $today_count; ?></strong>
+  </span>
+</div>
 
-                ?><a href="<?php echo $slug; ?>" class="a-row col-xs-12 col-ms-12 col-md-12 col-lg-12"><?php
-                    ?><div class='a-col col-xs-2 col-ms-2 col-md-2 col-lg-2'><?php echo $user; ?></div><?php
-                    ?><div class='a-col col-xs-2 col-ms-2 col-md-2 col-lg-2'><?php echo $report->name; ?></div><?php
-                    ?><div class='a-col col-xs-2 col-ms-2 col-md-2 col-lg-2'><?php echo $viewed; ?></div><?php
-                    ?><div class='a-col col-xs-2 col-ms-2 col-md-2 col-lg-3'><?php echo $report->create_date; ?></div><?php
-                ?></a><?php
+<div class="a-container col-lg-12">
+  <h2>Report overview</h2>
+  <div class="stat-container-a col-lg-12"> <?php
+    $reports = $report_control->get_all_reports();
+    $count = sizeof($reports);
+    $view_count = 0;
+    $today_count = 0;
 
-            }
-        ?>
-    </div>
-    <span class="analytics-stats">
-        Viewed reports: <strong><?php echo $report_view_count; ?></strong> / <?php echo $report_count; ?>
-    </span>
+    foreach($reports as $report) {
+      $user = $user_control->get((int)$client_control->get($report->client_id)->user_id)->name;
+      if (date('Ymd') == date('Ymd', strtotime($report->create_date))) {
+        $today_count++;
+      }
 
-    <span class="analytics-stats">
-        Reports created today: <strong><?php echo $report_today_count; ?></strong>
-    </span>
+      if($report->view_time == NULL) {
+        $viewed = "not viewed";
+      } else {
+        $viewed = "viewed";
+        $view_count++;
+      }
 
+      $slug = strtolower('report-'.$report->name.'-'.$report->id.'/'); ?>
+      <a href="<?php echo $slug; ?>" class="a-row col-xs-12 col-ms-12 col-md-12 col-lg-12">
+        <div class='a-col col-xs-2 col-ms-2 col-md-2 col-lg-2'><?php echo $user; ?></div>
+        <div class='a-col col-xs-2 col-ms-2 col-md-2 col-lg-2'><?php echo $report->name; ?></div>
+        <div class='a-col col-xs-2 col-ms-2 col-md-2 col-lg-2'><?php echo $viewed; ?></div>
+        <div class='a-col col-xs-2 col-ms-2 col-md-2 col-lg-3'><?php echo $report->create_date; ?></div>
+      </a><?php
+    } ?>
+  </div>
+
+  <span class="analytics-stats">
+    Viewed reports: <strong><?php echo $view_count; ?></strong> / <?php echo $count; ?>
+  </span>
+
+  <span class="analytics-stats">
+    Reports created today: <strong><?php echo $today_count; ?></strong>
+  </span>
 </div>
 
 </body>
