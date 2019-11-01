2379528bc764d3a1b03071fe2f0de8a7476789e2
Bram Hoogenkamp <bramhoogenkamp@live.nl>
2019-11-01 16:56:57 +0100

    Regex van client naam af

--- wp-content/themes/leadengine/dashboard/pages/setups/client_setup.php
+++ wp-content/themes/leadengine/dashboard/pages/setups/client_setup.php
@@ -53,7 +53,7 @@
         </div>
         <form class="col-lg-10 create_client_form" method="post" action="<?php echo $to; ?>">
           Name:<br>
-          <input maxlength="25" type="text" id="client_name" name="client_name" placeholder="Name" pattern="<?php echo $Regex->name ?>" title="Only letters are allowed" required>
+          <input maxlength="25" type="text" id="client_name" name="client_name" placeholder="Name" title="Only letters are allowed" required>
 
           Facebook Page:<br>
           <input type="text" id="facebook_url" data-type="facebook" name="facebook_url" placeholder="Facebook page url, page id or page username" required>
