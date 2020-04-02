5430d8608fca0565e85caeaf6758ac1fc7c7360f
Thomas Guntenaar <thomas@guntenaar.org>
2020-04-02 16:10:54 +0200

    False

--- wp-content/themes/leadengine/process_signature.php
+++ wp-content/themes/leadengine/process_signature.php
@@ -60,7 +60,7 @@ if( move_uploaded_file( $signature['tmp_name'], $new_file_path ) ) {
 		'post_title'     => preg_replace( '/\.[^.]+$/', '', $new_file_name ),
 		'post_content'   => '',
 		'post_status'    => 'inherit'
-  ), $false, 0, True);
+  ), False, 0, True);
   
   var_dump($upload_id);
   // TODO:
