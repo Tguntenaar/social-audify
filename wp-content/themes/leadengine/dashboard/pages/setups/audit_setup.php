f4b6600c39d6784686893fe0bf4d54eabca2067b
Daan Kuyper <daan_kuyper@hotmail.nl>
2020-10-28 16:05:45 +0100

    Audit Setup veel simpeler

--- wp-content/themes/leadengine/dashboard/pages/setups/audit_setup.php
+++ wp-content/themes/leadengine/dashboard/pages/setups/audit_setup.php
@@ -12,9 +12,19 @@
   <?php
     // Header
     include(dirname(__FILE__)."/../header/dashboard_header.php");
-    
+
+    $client_id = $_GET['cid'];
+
+    // Redirect to dashboard without client.
+    if (!isset($client_id)) {
+        header("Location: https://".getenv('HTTP_HOST')."/dashboard");
+    }
+
     $user = $user_control->get($user_id);
     $clients = $client_control->get_all();
+
+    $audits_made = $audit_control->get_amount(date('Y-m-d'));
+    $audits = $audit_control->get_all($user_id);
   ?>
   <head>
     <title>Create Audit</title>
@@ -24,172 +34,125 @@
 
   <div id="competitorModal" class="modal"></div>
   <div class="content-right y-scroll col-xs-12 col-sm-12 col-md-12 col-lg-12 responsive-padding" style="padding-bottom: 25px;">
-  <!-- back button -->
-  <div style="display: none;" class="content-title col-xs-12 col-sm-12 col-md-12 col-lg-12">
-      <span class="back" onclick="showIntro(true)">
-      <i class="fas fa-chevron-left"></i> Back</span>
-    <div class="audit_count"><?php
-      $max_audits = 20;
-      $audits_made = $audit_control->get_amount(date('Y-m-d'));
-      $audits = $audit_control->get_all($user_id);
-      echo $audits_made.'/'.$max_audits.' audits today'; ?>
-    </div>
-  </div>
 
-  <!-- Initial block -->
-  <div class="new-create-style create-block-box col-xs-12 col-sm-12 col-md-12 col-lg-12">
-    <?php
-    if ($audits_made >= $max_audits) { ?>
-      <div class="max-audits"> Max of <?php echo $max_audits; ?> audits reached.
-        <span class="description">You can upgrade your account to increase your daily audit count.</span>
-        <a href="../dashboard/"><i class="fas fa-th-large"></i>&nbsp; Dashboard</a>
-      </div><?php
-    } else { ?>
-      <h3>Existing or New contact?</h3>
-      <p>
-        Here you can choose to select a contact you made previously or simply make a completely new contact.
-        Selecting an existing contact will retrieve your contact list, which stores all contacts from previous audits and reports.
-        When creating a new contact you start from scratch and need to fill in a contact form, after which this new contact
-        will be added to your client list automatically for future use.
-      </p>
-      <div class="audit-option-center">
-        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 audit-option border-rightt audit-option-left" onclick="showIntro(false)">
-          <div class="vertical-align">
-            <span class="option-title">Existing contact</span>
-            <span class="option-text">Choose a client from your client list.</span>
+  <!-- Containers for multistep form -->
+  <div class="overview-audit-report col-xs-12 col-sm-12 col-md-12 col-lg-12">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 left responsive-height" style="height: 605px;">
+      <div class="inner">
+        <span class="title title-audit-page">Create an Audit</span>
+        <div class="audit_count"><?php echo $audits_made.' created Audits today'; ?></div>
+
+        <!-- One "tab" for each step in the form: -->
+        <form class="setup-form submit-audit">
+          <!-- Facebook tab -->
+          <div class="tab tab-setup">
+            <span class="login-title">Login to retrieve the data of Facebook that is needed to create an Audit.</span>
+            <span style="color: #c0392b; font-size: 12px; display: block;text-align: center; width: 100%;">Trouble logging in to Facebook? Try another browser.</span>
+            <div style="margin-top: 30px" class="fb-login-button login-center"
+                data-scope="manage_pages,instagram_basic,instagram_manage_insights,ads_read"
+                auth_type="rerequest"
+                data-width="100"
+                data-max-rows="1"
+                data-size="large"
+                data-button-type="continue_with"
+                data-show-faces="false"
+                data-auto-logout-link="true"
+                data-use-continue-as="false"
+                onlogin="checkLoginState();">
+            </div>
           </div>
-        </div>
-        <a href="../client-setup/?from=audit" class="ol-xs-12 col-sm-6 col-md-6 col-lg-6 audit-option audit-option-right">
-          <div class="vertical-align">
-            <span class="option-title">Create new contact</span>
-            <span class="option-text">Make a client and add it to your client list.</span>
+
+          <!-- Choose a client -->
+          <div class="tab tab-setup">
+            <div style="border: 0;" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 row-title no-padding">
+              <div style="font-weight: 100;" class="col-xs-10 col-sm-10 col-md-10 col-lg-10 row-title-style title-green no-padding">Select a contact</div>
+            </div>
+            <input type="text" name="search" class="setup-input" id="search-input" placeholder="Search..." valid/>
+            <div class="inner-scroll" style="height: 335px;" id="client-list"><?php 
+              foreach($clients as $client) {
+
+                $selected = $client->id == $client_id ? "selected" : "";
+                $data = ["id"=> $client->id, "name"=>$client->name, "facebook"=> $client->facebook, "instagram"=> $client->instagram, "website"=> $client->website]; ?>
+
+                <a class="col-xs-12 col-sm-12 col-md-12 col-lg-12 audit-row client campaign-row <?php echo $selected; ?>" 
+                   name="<?php echo $client->name; ?>" id="client-<?php echo $client->id;?>" data-client='<?php echo htmlentities(json_encode($data)); ?>'><?php
+
+                  echo "$client->name".($client->audit_count > 0 ? 
+                    "<div class='client-status converted'>Audit sent</div>" :
+                    "<div class='client-status no_reply'>New</div>"); ?>
+                </a><?php
+              } ?>
+            </div>
           </div>
-        </a>
+
+          <!-- Chosing an competitor -->
+          <div class="tab tab-setup" >
+            <span style="display: none;" class="temp-show-comp"></span>
+            <div style="border: 0;" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 row-title no-padding">
+              <div style="font-weight:100;" class="col-xs-10 col-sm-10 col-md-10 col-lg-10 row-title-style title-green no-padding">Want to compare <span style="font-weight: 100; color: #000;" class="show-client"></span> to a Competitor?<span style="color:#000; font-size: 12px; margin-left: 5px;">(Optional)</span></div>
+            </div>
+            <input type="text" name="search" class="setup-input" id="search-input-compare" placeholder="Search..." valid/>
+            <div class="inner-scroll" style="height: 335px;" id="compare-list">
+              <a class="col-xs-12 col-sm-12 col-md-12 col-lg-12 audit-row client selected" name="-" data-compare=''>No competitor...</a>
+              <a class="col-xs-12 col-sm-12 col-md-12 col-lg-12 audit-row new-compare" name="+"
+                data-compare='<?php echo htmlentities(json_encode([ 'facebook' => '', 'instagram' => '', 'website' => ''])); ?>'>New competitor
+              </a><?php
+              foreach($clients as $client) {
+                $data = ["id"=> $client->id, "name"=> $client->name, "facebook"=> $client->facebook, "instagram"=> $client->instagram, "website"=> $client->website];?>
+                <a class="col-xs-12 col-sm-12 col-md-12 col-lg-12 audit-row client campaign-row" name="<?php echo $client->name; ?>"
+                  data-compare='<?php echo htmlentities(json_encode($data)); ?>'><?php echo $client->name; ?>
+                </a><?php
+              } ?>
+            </div>
+          </div>
+
+          <!-- Audit name and options -->
+          <div class="tab tab-setup">
+            <div class="custom-label">
+              <span style="font-weight: 100;" class="name-label">Fill in your audit name</span>
+              <input type="text" id="name-input" name="audit_name" class="setup-input name-input" placeholder="Audit name.." title="Only letters and numbers are allowed." maxlength="25" required>
+            </div>
+            <div class="custom-label">
+              <span style="font-weight: 100;" class="name-label" style="margin-top: -15px;">Audit options:</span>
+              <span class="notice"><p>All the options are selected to include the data of the following platforms, you can deselect it by clicking on the icons.</p></span>
+              <label class="c_container">
+                <input type="checkbox" name="facebook_checkbox" id="facebook_checkbox" value="facebook_checkbox" checked><br />
+                <span class="checkmark"><i class="fab fa-facebook-f"></i></span>
+              </label>
+              <label class="c_container">
+                <input type="checkbox" name="instagram_checkbox" id="instagram_checkbox" value="instagram_checkbox" checked><br />
+                <span class="checkmark"><i class="fab fa-instagram"></i></span>
+              </label>
+              <label class="c_container">
+                <input type="checkbox" name="website_checkbox"  id="website_checkbox" value="website_checkbox" checked><br />
+                <span class="checkmark"><i class="fas fa-globe"></i></span>
+              </label>
+            </div>
+            <div style="clear: both;"></div>
+            <span style="font-weight: 100;" class="name-label" style="margin-top: -20px; float: left;">Selected contact:</span>
+            <div style="font-size: 14px !important; position: absolute; left: 20px; margin-top: 5px; float: left;">
+              <span style="font-weight: 600;" class="show-client"></span>
+              <span class="has_comp"></span><span style="font-weight: 600;" class="show-compare"></span>
+            </div>
+            <br/>
+          </div>
+          <div>
+            <div class="nav-buttons">
+              <button type="button" id="prevBtn" onclick="nextPrev(-1)">Previous</button>
+              <button type="button" id="nextBtn" onclick="nextPrev(1)">Next</button>
+            </div>
+          </div>
+
+          <!-- Circles that indicate form steps: -->
+          <div class="dot-nav" style="text-align:center;">
+            <span class="step"></span>
+            <span class="step"></span>
+            <span class="step"></span>
+            <span class="step"></span>
+          </div>
+        </form>
       </div>
     </div>
-    <!-- Containers for multistep form -->
-    <div class="overview-audit-report col-xs-12 col-sm-12 col-md-12 col-lg-12">
-      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 left responsive-height" style="height: 605px;">
-        <div class="inner">
-          <span class="title title-audit-page">Create an Audit</span>
-          <div class="audit_count"><?php
-            echo $audits_made.'/'.$max_audits.' audits today'; ?>
-          </div>
-
-          <!-- One "tab" for each step in the form: -->
-          <form class="setup-form submit-audit">
-            <!-- Facebook tab -->
-            <div class="tab tab-setup">
-              <span class="login-title">Login to retrieve the data of Facebook that is needed to create an Audit.</span>
-              <span style="color: #c0392b; font-size: 12px; display: block;text-align: center; width: 100%;">Trouble logging in to Facebook? Try another browser.</span>
-              <div style="margin-top: 30px" class="fb-login-button login-center"
-                  data-scope="manage_pages,instagram_basic,instagram_manage_insights,ads_read"
-                  auth_type="rerequest"
-                  data-width="100"
-                  data-max-rows="1"
-                  data-size="large"
-                  data-button-type="continue_with"
-                  data-show-faces="false"
-                  data-auto-logout-link="true"
-                  data-use-continue-as="false"
-                  onlogin="checkLoginState();">
-              </div>
-            </div>
-
-            <!-- Choose a client -->
-            <div class="tab tab-setup">
-              <div style="border: 0;" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 row-title no-padding">
-                <div style="font-weight: 100;" class="col-xs-10 col-sm-10 col-md-10 col-lg-10 row-title-style title-green no-padding">Select a contact</div>
-              </div>
-              <input type="text" name="search" class="setup-input" id="search-input" placeholder="Search..." valid/>
-              <div class="inner-scroll" style="height: 335px;" id="client-list"><?php 
-                if($clients != NULL) {
-                  foreach($clients as $client) {
-
-                    $data = ["id"=> $client->id, "name"=>$client->name, "facebook"=> $client->facebook, "instagram"=> $client->instagram, "website"=> $client->website]; ?>
-                    <a class="col-xs-12 col-sm-12 col-md-12 col-lg-12 audit-row client campaign-row" name="<?php echo $client->name; ?>" id="client-<?php echo $client->id;?>"
-                      data-client='<?php echo htmlentities(json_encode($data)); ?>'><?php
-                      echo "$client->name".($client->audit_count > 0 ? 
-                        "<div class='client-status converted'>Audit sent</div>" :
-                        "<div class='client-status no_reply'>New</div>"); ?>
-                    </a><?php
-                  }
-                } else { ?>
-                  <a href="/client-dashboard">Create a client first.</a><?php
-                } ?>
-              </div>
-            </div>
-
-            <!-- Chosing an competitor -->
-            <div class="tab tab-setup" >
-              <span style="display: none;" class="temp-show-comp"></span>
-              <div style="border: 0;" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 row-title no-padding">
-                <div style="font-weight:100;" class="col-xs-10 col-sm-10 col-md-10 col-lg-10 row-title-style title-green no-padding">Want to compare <span style="font-weight: 100; color: #000;" class="show-client"></span> to a Competitor?<span style="color:#000; font-size: 12px; margin-left: 5px;">(Optional)</span></div>
-              </div>
-              <input type="text" name="search" class="setup-input" id="search-input-compare" placeholder="Search..." valid/>
-              <div class="inner-scroll" style="height: 335px;" id="compare-list">
-                <a class="col-xs-12 col-sm-12 col-md-12 col-lg-12 audit-row client selected" name="-" data-compare=''>No competitor...</a>
-                <a class="col-xs-12 col-sm-12 col-md-12 col-lg-12 audit-row new-compare" name="+"
-                  data-compare='<?php echo htmlentities(json_encode([ 'facebook' => '', 'instagram' => '', 'website' => ''])); ?>'>New competitor
-                </a><?php
-                foreach($clients as $client) {
-                  $data = ["id"=> $client->id, "name"=> $client->name, "facebook"=> $client->facebook, "instagram"=> $client->instagram, "website"=> $client->website];?>
-                  <a class="col-xs-12 col-sm-12 col-md-12 col-lg-12 audit-row client campaign-row" name="<?php echo $client->name; ?>"
-                    data-compare='<?php echo htmlentities(json_encode($data)); ?>'><?php echo $client->name; ?>
-                  </a><?php
-                } ?>
-              </div>
-            </div>
-
-            <!-- Audit name and options -->
-            <div class="tab tab-setup">
-              <div class="custom-label">
-                <span style="font-weight: 100;" class="name-label">Fill in your audit name</span>
-                <input type="text" id="name-input" name="audit_name" class="setup-input name-input" placeholder="Audit name.." title="Only letters and numbers are allowed." maxlength="25" required>
-              </div>
-              <div class="custom-label">
-                <span style="font-weight: 100;" class="name-label" style="margin-top: -15px;">Audit options:</span>
-                <span class="notice"><p>All the options are selected to include the data of the following platforms, you can deselect it by clicking on the icons.</p></span>
-                <label class="c_container">
-                  <input type="checkbox" name="facebook_checkbox" id="facebook_checkbox" value="facebook_checkbox" checked><br />
-                  <span class="checkmark"><i class="fab fa-facebook-f"></i></span>
-                </label>
-                <label class="c_container">
-                  <input type="checkbox" name="instagram_checkbox" id="instagram_checkbox" value="instagram_checkbox" checked><br />
-                  <span class="checkmark"><i class="fab fa-instagram"></i></span>
-                </label>
-                <label class="c_container">
-                  <input type="checkbox" name="website_checkbox"  id="website_checkbox" value="website_checkbox" checked><br />
-                  <span class="checkmark"><i class="fas fa-globe"></i></span>
-                </label>
-              </div>
-              <div style="clear: both;"></div>
-              <span style="font-weight: 100;" class="name-label" style="margin-top: -20px; float: left;">Selected contact:</span>
-              <div style="font-size: 14px !important; position: absolute; left: 20px; margin-top: 5px; float: left;">
-                <span style="font-weight: 600;" class="show-client"></span>
-                <span class="has_comp"></span><span style="font-weight: 600;" class="show-compare"></span>
-              </div>
-              <br/>
-            </div>
-            <div>
-              <div class="nav-buttons">
-                <button type="button" id="prevBtn" onclick="nextPrev(-1)">Previous</button>
-                <button type="button" id="nextBtn" onclick="nextPrev(1)">Next</button>
-              </div>
-            </div>
-
-            <!-- Circles that indicate form steps: -->
-            <div class="dot-nav" style="text-align:center;">
-              <span class="step"></span>
-              <span class="step"></span>
-              <span class="step"></span>
-              <span class="step"></span>
-            </div>
-          </form>
-        </div>
-      </div><?php
-    } ?>
-  </div>
   </div>
   <script>
     // Audit Instance - filled in multistep.
@@ -205,10 +168,6 @@
       ?>,
     }
 
-    $("body").on('DOMSubtreeModified', ".show-compare", function() {
-        $('.has_comp').html(" is going to be compared to ");
-    });
-
     // Selectable list - TODO : kan wss naar dashboard-header
     $('.audit-row.client').on('click', function() {
       $(this).parent().find('.audit-row').removeClass('selected');
@@ -216,22 +175,12 @@
       nextPrev(1);
     });
 
+    // Custom 'Selected contact'-message when competitor is selected.
+    $("body").on('DOMSubtreeModified', ".show-compare", function() {
+        $('.has_comp').html(" is going to be compared to ");
+    });
+
     $(function() {
-      <?php
-      if (isset($_GET['cid'])) { ?>
-        showIntro(false);
-        // Bram
-        // nextPrev(2);
-        
-
-        var id = "<?php echo $_GET['cid']; ?>";
-        var selected = $(`#client-list a[id=client-${id}]`);
-        console.log(selected);
-        // wrm zou iets geselecteerd zijn?
-        selected.parent().find('.audit-row').removeClass('selected');
-        selected.addClass('selected');<?php
-      } ?>
-
       // New Competitor Modal:
       var modalData = {
         text: 'New Competitor:',
